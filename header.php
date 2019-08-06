ea867a172153e5de60aee64067c5bfd874cdf138
Dustin Jones <info@domesticjones.com>
2019-08-06 13:41:43 -0600

    style header navigation, header brand

--- header.php
+++ header.php
@@ -10,30 +10,28 @@
 		<div id="container">
 			<header id="tagline"><h1><?php bloginfo('description'); ?></h1></header>
       <header id="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
-        <div class="wrap">
-          <a href="<?php echo get_home_url(); ?>">
-						<img src="<?php ex_logo(); ?>" alt="Logo for <?php ex_brand(); ?>" class="logo-header" />
-					</a>
-          <nav class="nav-header" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-            <?php wp_nav_menu(array(
-              'container' => false,								// remove nav container
-              'container_class' => '',						// class of container (should you choose to use it)
-              'menu' => __('Header', 'exonym'),	  // nav name
-              'menu_class' => '',									// adding custom nav class
-              'theme_location' => 'header-menu',	// where it's located in the theme
-              'before' => '',											// before the menu
-              'after' => '',											// after the menu
-              'link_before' => '',								// before each link
-              'link_after' => '',									// after each link
-              'depth' => 0,												// limit the depth of the nav
-              'fallback_cb' => ''									// fallback function (if there is one)
-            )); ?>
-          </nav>
-          <?php ex_social(); ?>
-					<a href="#" id="responsive-nav-toggle">
-	          <span class="line"></span>
-	          <span class="line"></span>
-	          <span class="line"></span>
-					</a>
-        </div>
+        <a href="<?php home_url(); ?>">
+					<img src="<?php ex_logo('primary', 'light'); ?>" alt="Logo for <?php ex_brand(); ?>" class="header-logo" />
+				</a>
+        <nav class="header-nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+          <?php wp_nav_menu(array(
+            'container' => false,								// remove nav container
+            'container_class' => '',						// class of container (should you choose to use it)
+            'menu' => __('Header', 'exonym'),	  // nav name
+            'menu_class' => '',									// adding custom nav class
+            'theme_location' => 'header-menu',	// where it's located in the theme
+            'before' => '',											// before the menu
+            'after' => '',											// after the menu
+            'link_before' => '',								// before each link
+            'link_after' => '',									// after each link
+            'depth' => 0,												// limit the depth of the nav
+            'fallback_cb' => ''									// fallback function (if there is one)
+          )); ?>
+        </nav>
+        <?php ex_social(); ?>
+				<a href="#" id="header-nav-toggle">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+				</a>
       </header>
